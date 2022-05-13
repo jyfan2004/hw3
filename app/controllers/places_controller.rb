2e7c2e4cc07d8a944ac0e592581057551b3f8bf6
jyfan2004 <james.fan@kellogg.northwestern.edu>
2022-05-13 02:31:04 +0000

    Working on passing places id to hidden field for post

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,7 +7,7 @@ class PlacesController < ApplicationController
 
     def show
         @place = Place.find_by({ "id" => params["id"] })
-
+        
     end
 
     def new
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,13 @@
 class PostsController < ApplicationController
+
+    def index
+    end
+
+    def new
+        @post = Post.new
+        @post["company_id"] = params["company_id"]
+    end
+
+
+
 end
